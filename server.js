8a8e27caa6f7f0e28c8ac5578ad42b32a68a109f
Collin Sherriff <collin@fanfire.ai>
2023-05-17 18:36:12 +0400

    new edits

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
-js
-// server.js
-const express = require('express');
+const express = require('express');  
 const unzipper = require('unzipper');
-const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+const axios = require('axios');  
 
-app.post('/file/post', (req, res) => {
-  const zipFile = req.files.file;
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use(express.json({ limit: '50mb' }));  
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));   
+
+app.post('/file/post', async (req, res) => {  
+  const zipFile = req.files.file;   
   
   zipFile.mv(`./${zipFile.name}`, err => {
-    if (err) res.status(500).send(err);
+    if (err) return res.status(500).send(err);   
     
-    unzipper.Open(`./${zipFile.name}`).then(zip => {
-      zip.extract({
-        path: './extracted'  // extract files into ./extracted folder
-      }); 
-      zip.close();
+    unzipper.Open(`./${zipFile.name}`).then(async zip => {    
+      zip.extract({          
+        path: './extracted'  
+      });       
+      zip.close();  
+           
+      // Call Claude API with extracted files
+      let files = require('fs').readdirSync('./extracted');
+      let results = await axios.post('https://claude.ai/analyze', { files }); 
       
-      // Use the extracted files to call Claude API...
-      // Save results... 
+      // Save results
+      // ... 
       
-      res.send('File uploaded and extracted!'); 
+      res.send(results.data); 
     });
   });
 });
