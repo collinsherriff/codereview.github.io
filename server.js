b7ffbd045452d4d9aa0b4671ea3c7fd830b56cd5
Collin Sherriff <collin@fanfire.ai>
2023-05-18 10:30:02 +0400

    api calls

--- server.js
+++ server.js
@@ -14,66 +14,80 @@ app.get('/', (req, res) => {
 
 const app = express();
 
+// Enable CORS 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  console.log(req);
-  console.log(res);
   next();    
 });
 
+// Validate and handle file upload
+function validateFile(req, res, next) {
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  
+  next(); 
+}
+
 app.use(express.json({ limit: '50mb' }));    
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));       
-
-app.post('/file/upload', async (req, res) => {      
+app.post('/file/upload', validateFile, async (req, res) => {      
   console.log(req);   
   
   const zipFile = req.files.file;       
   
   zipFile.mv(`./${zipFile.name}`, err => {
     if (err) return res.status(500).send(err);
-    
-    unzipper.Open(`./${zipFile.name}`).then(async zip => {  
-      zip.extract({            
-        path: './extracted'    
-      });       
-      zip.close();    
-      let filesText = '';
-      let dirs = fs.readdirSync('./extracted', {withFileTypes: true});
-      for (let dir of dirs) {
-        if (dir.isDirectory()) {  
-          let subdir = fs.readdirSync(`./extracted/${dir.name}`);
-          filesText += `${dir.name}\n`;  
-          for (let file of subdir) {
-            filesText += `  ${file}\n`;       
-            filesText += fs.readFileSync(`./extracted/${dir.name}/${file}`, 'utf8');
-            filesText += '\n\n';
-          } 
-        }
-      }   
-      let prompt = `\n\nHuman: Analyse the code\n\nAssistant: ${filesText}`;  
-     client  
-          .completeStream(
-            {
-              prompt,  
-              stop_sequences: ["\n\nHuman:"],
-              max_tokens_to_sample: 300, 
-              model: "claude-v1",
-            },
-            {
-              onOpen: (response) => {
-                console.log("Opened stream, HTTP status code", response.status);
-              },
-              onUpdate: (completion) => {
-                console.log(completion.completion);
-              },
-            }
-          )
-          .then((completion) => {
-            console.log("Finished sampling:\n", completion);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-    });
   });
-}); 
+  
+  // Extract zip file 
+  unzipper.Open(`./${zipFile.name}`).then(async zip => {  
+    zip.extract({            
+      path: './extracted'    
+    });       
+    zip.close();    
+  });
+  
+  // Get text from all files
+  let filesText = '';
+  let dirs = fs.readdirSync('./extracted', {withFileTypes: true});
+  for (let dir of dirs) {
+    if (dir.isDirectory()) {  
+      let subdir = fs.readdirSync(`./extracted/${dir.name}`);
+      filesText += `${dir.name}\n`;  
+      for (let file of subdir) {
+        filesText += `  ${file}\n`;       
+        filesText += fs.readFileSync(`./extracted/${dir.name}/${file}`, 'utf8');
+        filesText += '\n\n';
+      } 
+    }
+  }  
+  
+  // Call Claude API
+  const userQuestion = `Analyse the code ${filesText}`;
+  const prompt = `\n\nHuman: ${userQuestion}\n\nAssistant: `; 
+  client  
+      .completeStream(
+        {
+          prompt,  
+          stop_sequences: ["\n\nHuman:"],
+          max_tokens_to_sample: 300, 
+          model: "claude-v1",
+        },
+        {
+          onOpen: (response) => {
+            console.log("Opened stream, HTTP status code", response.status);
+          },
+          onUpdate: (completion) => {
+            console.log(completion.completion);
+          },
+        }
+      )
+      .then((completion) => {
+        console.log("Finished sampling:\n", completion);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+});
